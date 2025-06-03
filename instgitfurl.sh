42bde784068d08d0fdcdd9779fdae7cf2564808b
cheezitlinreturns <ethan.arnoldddd12314@gmail.com>
2025-06-03 03:01:08 -0500

    add the installler and program

---
+++ instgitfurl.sh
@@ -0,0 +1,6 @@
+# installer for GIPFURL
+echo "please run this as sudo, i am too lazy to add a check."
+cp gipfurl /usr/local/bin/gipfurl
+echo "done."
+echo "to use gipfurl, simply type gipfurl followed by the URLs of the places you want to get the IPs of. you can do multiple IPs at once."
+exit
